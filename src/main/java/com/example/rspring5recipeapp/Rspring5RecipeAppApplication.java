b33e19d336f962dd446f2f62e9235becd5a5d9d2
xTres1985 <boguslaw.borozynski@gmail.com>
2021-03-20 20:17:04 +0100

    initial commit

---
+++ src/main/java/com/example/rspring5recipeapp/Rspring5RecipeAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.rspring5recipeapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Rspring5RecipeAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Rspring5RecipeAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/rspring5recipeapp/Rspring5RecipeAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.rspring5recipeapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Rspring5RecipeAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
